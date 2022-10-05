c2103c1fb7fc7a5cb15ab5721c893e97ad4ed1e8
DineshZingade <zingadedinesh1991@gmail.com>
2022-10-05 11:07:40 +0530

    Added Test Case to Return SAD

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.TC1Returnsad;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+public class MoodAnalyserTest extends TestCase {
+	public void testAnalyseMood() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		moodAnalyser.setMessage("I am in Sad Mood");
+		String actualResult = moodAnalyser.analyseMood();
+		Assert.assertEquals("SAD", actualResult);
+	}
+}
